0102a4107af067c3d3e84f673223f651d3df5b90
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-30 09:05:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length < 2 
+    array.join
+    elsif array.length == 2
+    array.join(' and ')
+  else
+    w = array.pop
+    strings = array.join (', ')
+    strings + ", and " + w 
+  end 
+ 
 end
