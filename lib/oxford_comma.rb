a9c7af7d26b16c0d5b0c39a0294c00dee8eb7f00
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-30 08:34:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
 def oxford_comma(array)
-  if array.length == 1 
+  if array.length < 2 
     array.join
     elsif array.length == 2
     array.join(' and ')
-    elsif
-    
-    
- 
- 
+  else
+    string = array.pop
+    (array.pop.join (', ')) << "and #{string}"
+  end 
  
 end
